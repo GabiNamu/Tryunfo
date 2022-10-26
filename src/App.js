98f01e42e5a63cf6385ada164b5501d91924b4d2
Gabriela Muniz <gabrielanamu@live.com>
2022-10-26 19:29:10 -0300

    adiciona requisito 8

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1,
       cardAttr2, cardAttr3, cardImage, cardRare, cardTrunfo,
-      isSaveButtonDisabled, hasTrunfo } = this.state;
+      isSaveButtonDisabled, hasTrunfo, savedCard } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -107,6 +107,25 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        <ul>
+          {
+            savedCard !== []
+              ? savedCard.map((card) => (
+                <li key={ card.cardName }>
+                  <Card
+                    cardName={ card.cardName }
+                    cardDescription={ card.cardDescription }
+                    cardAttr1={ card.cardAttr1 }
+                    cardAttr2={ card.cardAttr2 }
+                    cardAttr3={ card.cardAttr3 }
+                    cardImage={ card.cardImage }
+                    cardRare={ card.cardRare }
+                    cardTrunfo={ card.cardTrunfo }
+                  />
+                </li>))
+              : ''
+          }
+        </ul>
       </div>
     );
   }
